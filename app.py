9ece9ab303ffc260d6bd5d9a33cc7017c96253b7
muayjon <51769644+muayjon@users.noreply.github.com>
2019-06-13 09:13:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     a=os.environ['Authorization']
-    return a
+    return "หมวย จอน 4/7 24"
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
